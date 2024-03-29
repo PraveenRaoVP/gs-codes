db44aeeb232efaf5812c786511edc20fd8fac486
Praveen Rao V P <praveenraovp01@gmail.com>
2024-03-29 20:11:39 +0530

    assignment 10 player question updated

--- assignments/assignment10_interfaces/PlayerDemo.java
+++ assignments/assignment10_interfaces/PlayerDemo.java
@@ -1,5 +1,7 @@
 package assignments.assignment10_interfaces;
 
+import java.util.Scanner;
+
 /*Define an interface named Playable with methods like play() , pause() , and stop() . Implement this interface in classes representing different types of music players such as MP3Player , CDPlayer , and StreamingPlayer . Allow the user to control the playback of these players through the implemented interface. */
 
 interface Playable {
@@ -64,19 +66,54 @@ class StreamingPlayer implements Playable {
 
 public class PlayerDemo {
     public static void main(String[] args) {
-        Playable mp3Player = new MP3Player();
-        mp3Player.play();
-        mp3Player.pause();
-        mp3Player.stop();
+        System.out.println("Enter your choice: ");
+        System.out.println("1. MP3 Player");
+        System.out.println("2. CD Player");
+        System.out.println("3. Streaming Player");
+        Scanner sc = new Scanner(System.in);
+        Playable player = null;
 
-        Playable cdPlayer = new CDPlayer();
-        cdPlayer.play();
-        cdPlayer.pause();
-        cdPlayer.stop();
+        switch (sc.nextInt()) {
+            case 1:
+                player = new MP3Player();
+                break;
+            case 2:
+                player = new CDPlayer();
+                break;
+            case 3:
+                player = new StreamingPlayer();
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+        showMenu();
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                player.play();
+                break;
+            case 2:
+                player.pause();
+                break;
+            case 3:
+                player.stop();
+                break;
+            case 4:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+        sc.close();
+    }
 
-        Playable streamingPlayer = new StreamingPlayer();
-        streamingPlayer.play();
-        streamingPlayer.pause();
-        streamingPlayer.stop();
+    public static void showMenu() {
+        System.out.println("Enter your choice: ");
+        System.out.println("1. Play");
+        System.out.println("2. Pause");
+        System.out.println("3. Stop");
+        System.out.println("4. Exit");
     }
 }
